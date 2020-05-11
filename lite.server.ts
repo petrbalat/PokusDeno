5758f39e5fa19eb8c474d21bf25bcd2e58e89474
Petr Balat <petr.balat@gmail.com>
2020-05-11 13:42:36 +0200

    update 0.56

--- lite.server.ts
+++ lite.server.ts
@@ -1,8 +1,7 @@
-import {serve} from 'https://deno.land/std@v0.38.0/http/server.ts';
+import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
 
-const port = 8080;
-console.log(`http://localhost:${port}`);
-for await (const req of serve({port})) {
-    console.info(`${req.method} ${req.url}`)
-    req.respond({body: 'Hello World\n'});
+const s = serve({ port: 8000 });
+console.log("http://localhost:8000/");
+for await (const req of s) {
+    req.respond({ body: "Hello World\n" });
 }
