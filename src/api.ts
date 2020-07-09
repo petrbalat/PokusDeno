43df6744f9ded3145e90e5291cb0ccff6986af08
Petr Balat <petr.balat@gmail.com>
2020-07-09 17:43:10 +0200

    refac api

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,34 @@
 import api from 'https://deno.land/x/api/index.ts' // For Ts
 // import api from 'https://deno.land/x/api/index.js' // For JS
 
-console.log(await api.get('https://jsonplaceholder.typicode.com/todos/'))
+interface User {
+    userId: number;
+    id: number;
+    title: string,
+    completed: boolean
+}
 
-console.log(await api.getone('https://jsonplaceholder.typicode.com/todos/', 1))
+// const pom: any = await api.get('');
 
-console.log(await api.post('https://jsonplaceholder.typicode.com/todos/', {
+const users: User[] = await api.get('https://jsonplaceholder.typicode.com/todos/?completed=true');
+// console.log(users)
+
+const user1: User = await api.get('https://jsonplaceholder.typicode.com/todos/1');
+// console.log(user1)
+
+const insert: User = await api.post('https://jsonplaceholder.typicode.com/todos/', {
     userId: 5,
     title: "excepturi a et neque qui expedita vel voluptate",
     completed: false
-}))
-console.log(await api.delete('https://jsonplaceholder.typicode.com/todos/1'))
-console.log(await api.put('https://jsonplaceholder.typicode.com/todos/1', {
-    userId: 1,
+});
+// console.log(insert)
+
+const delete1: User = await api.delete('https://jsonplaceholder.typicode.com/todos/1');
+// console.log(delete1)
+
+const update = await api.put('https://jsonplaceholder.typicode.com/todos/109', {
+    userId: 109,
     title: "one",
     completed: true
-}))
+});
+console.log(update)
